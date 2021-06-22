d1df997f8cbaa0598467f4f9c7cc46944652e6f0
Rengga Maulana <renggaa21@gmail.com>
2021-06-22 08:25:41 +0700

    menambahkan halaman admin pada routes

--- Frontend/src/Routes/Index.svelte
+++ Frontend/src/Routes/Index.svelte
@@ -3,12 +3,14 @@
   import Home from "../Pages/Home.svelte";
   import Login from "../Pages/Login.svelte";
   import Checkout from "../Pages/Checkout.svelte";
+  import Admin from "../Pages/Admin.svelte";
 
   let page;
 
   router("/", () => (page = Home));
   router("/login", () => (page = Login));
   router("/checkout", () => (page = Checkout));
+  router("/admin", () => (page = Admin));
 
   router.start();
 </script>
