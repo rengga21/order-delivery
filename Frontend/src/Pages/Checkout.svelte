9c93e45ce9495e6751e8ea27248f46d3781a09c0
Rengga Maulana <renggaa21@gmail.com>
2021-06-24 14:03:01 +0700

    Merubah tampilan checkout

--- Frontend/src/Pages/Checkout.svelte
+++ Frontend/src/Pages/Checkout.svelte
@@ -1,107 +1,82 @@
 <script>
+  import Footer from "../Components/Footer.svelte";
   import Nav from "../Components/Nav.svelte";
 </script>
 
 <Nav />
 <div class="container">
-  <div class="row text-center mt-5">
-    <div class="col">
-      <!-- Informasi Pemesan -->
-      <h2>Informasi Pemesan</h2>
-    </div>
-  </div>
-  <div class="row justify-content-center mt-4">
-    <div class="col-md-6">
+  <div class="row mt-4">
+    <!-- Informasi Pemesan -->
+    <div class="col-md-7">
+      <h2>Informasi Pemesanan</h2>
       <form class="row g-3">
         <div class="col-md-6">
-          <label for="name" class="form-label">Nama Lengkap</label>
+          <label for="name" class="form-label">Nama</label>
           <input type="text" class="form-control" id="name" />
         </div>
         <div class="col-md-6">
           <label for="email" class="form-label">Email</label>
           <input type="email" class="form-control" id="email" />
         </div>
-        <label for="phone" class="form-label">No. Telp</label>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="phone ">+62</span>
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </div>
         <div class="col-12">
-          <label for="inputAddress" class="form-label">Alamat Pengiriman</label>
+          <label for="inputAddress" class="form-label">Alamat</label>
           <input type="text" class="form-control" id="inputAddress" />
         </div>
-
+        <div class="col-12">
+          <label for="inputAddress2" class="form-label"
+            >Kirim ke alamat lain</label
+          >
+          <input type="text" class="form-control" id="inputAddress2" />
+        </div>
         <div class="col-md-6">
-          <label for="inputCity" class="form-label">Kota</label>
+          <label for="inputCity" class="form-label">City</label>
           <input type="text" class="form-control" id="inputCity" />
         </div>
         <div class="col-md-4">
-          <label for="inputState" class="form-label">Provinsi</label>
+          <label for="inputState" class="form-label">State</label>
           <select id="inputState" class="form-select">
             <option selected>Choose...</option>
             <option>...</option>
           </select>
         </div>
         <div class="col-md-2">
-          <label for="inputZip" class="form-label">Kode Pos</label>
+          <label for="inputZip" class="form-label">Zip</label>
           <input type="text" class="form-control" id="inputZip" />
-        </div>
-
-        <!-- Metode Pembayaran -->
-
-        <div class="row mt-5">
-          <div class="div col-md-12">
-            <h3>Metode Pembayaran</h3>
-            <select id="inputState" class="form-select my-4">
-              <option selected>Pembayaran Online</option>
-              <option>Cash On Delivery</option>
-            </select>
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                id="flexRadioDefault1"
-              />
-              <label class="form-check-label" for="flexRadioDefault1">
-                Bank BCA
-              </label>
-            </div>
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="flexRadioDefault"
-                id="flexRadioDefault1"
-              />
-              <label class="form-check-label" for="flexRadioDefault1">
-                Bank BNI
-              </label>
-            </div>
-          </div>
         </div>
         <div class="col-12">
           <div class="form-check">
             <input class="form-check-input" type="checkbox" id="gridCheck" />
             <label class="form-check-label" for="gridCheck">
-              Pilih untuk menunjukkan kalau kamu sudah membaca dan menyetujui
-              <span class="term"> Terms of use</span> &
-              <span class="term"> Privacy policy</span> kami.
+              Check me out
             </label>
           </div>
         </div>
-        <div class="col-12 mb-5">
-          <button type="submit" class="btn btn-danger">Pesan Sekarang</button>
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Sign in</button>
         </div>
       </form>
+
+      <!-- Informasi Pengiriman -->
+      <h2>Informasi Pengiriman</h2>
+    </div>
+
+    <!-- Ringkasan Belanja -->
+    <div class="col-md-5">
+      <h2>Ringkasan Belanja</h2>
+      <table class="table-warning">
+        <th>Produk</th>
+        <tr>
+          <td>Produk</td>
+          <td>Qty</td>
+          <td>Harga</td>
+        </tr>
+      </table>
+      <!-- Metode Pembayaran -->
+      <h2>Pilih Metode Pembayaran</h2>
     </div>
   </div>
 </div>
+<Footer />
 
 <style>
   .container {
